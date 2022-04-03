49a2cefedc01141eda3290e104142f78a7e75969
FAUCITANO Liam <liam.faucitano@gmail.com>
2022-04-03 21:31:45 +0200

    Update for build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title:string = 'angular-gsb';
-  photo = 'https://images.unsplash.com/photo-1643965304951-07a7ebb783a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+  photo = 'https://images.unsplash.com/photo-1643965304951-07a7ebb783a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80';
+  public LOGO = "./assets/images/logo_top.png";
+  constructor() {};
 }
 
